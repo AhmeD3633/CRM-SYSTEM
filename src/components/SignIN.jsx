835c692274cb43cf059940cf684ca7c705bc7dbd
amrelhendawey <amrfouadfahmy7@gmail.com>
2024-10-28 19:30:30 +0300

    ..

--- src/components/SignIN.jsx
+++ src/components/SignIN.jsx
@@ -32,12 +32,19 @@ const SignIN = () => {
               </a>
             </div>
 
-            <form action="#" className="mt-8 grid grid-cols-1 gap-6">
+            <form action="#" className="grid grid-cols-1 gap-1">
+              <div className="relative w-full h-full  ml-auto pb-8 pt-8 rounded-lg">
+                <img
+                  alt=""
+                  src="./src/assets/logo.png"
+                  className="absolute top-4 right-0 w-32"
+                />
+              </div>
               <div className="col-span-1">
                 <img src="./src/assets/Logo.png" alt="" />
               </div>
               <div className="col-span-1">
-                <h1 className="text-2xl text-right">أنشئ حساب</h1>
+                <h1 className="text-4xl text-right">أنشئ حساب</h1>
                 <h2 className="text-lg text-right  text-slate-500 ">
                   أنشئ حساب الان لمتابعة اعمالك
                 </h2>
@@ -121,8 +128,9 @@ const SignIN = () => {
               </div>
               <div className="col-span-1">
                 <button
+                  style={{ backgroundColor: "#00FFA9" }}
                   type="submit"
-                  className="mt-6 w-full h-12  rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500"
+                  className="mt-6 w-full h-12 rounded-md px-4 py-2 text-sm font-semibold text-black shadow-sm hover:bg-blue-500"
                 >
                   تسجيل الدخول
                 </button>
